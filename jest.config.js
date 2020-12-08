6db3218561286777e0cf2fa350d040e4d52c602a
SinshiSmith <yorfnd@gmail.com>
2020-12-08 19:10:47 +0200

    Adjust jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
