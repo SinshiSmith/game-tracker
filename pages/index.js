fccdc475da375a8c269b30ec007ad3d613db2204
SinshiSmith <yorfnd@gmail.com>
2020-12-22 15:07:15 +0200

    /home: wrap games in infinte loader

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,56 @@
-const Home = ({ games }) => (
-  <main css={{ padding: 10 }}>
-    <div css={{ textAlign: "center" }}>
-      <h1 css={{ margin: 0, fontWeight: 700, fontSize: 70 }}>Game Tracker</h1>
-      <p css={{ marginTop: 0 }}>a website to keep track of my games</p>
-    </div>
-    <div css={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-      {games.results.map((game) => (
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import Loader from "../components/Loader";
+
+const Home = ({ games = {} }) => {
+  const [data, setData] = useState(games);
+  return (
+    <main css={{ padding: 10 }}>
+      <div css={{ textAlign: "center" }}>
+        <h1 css={{ margin: 0, fontWeight: 700, fontSize: 70 }}>Game Tracker</h1>
+        <p css={{ marginTop: 0 }}>a website to keep track of my games</p>
+      </div>
+      <InfiniteScroll
+        pageStart={0}
+        hasMore={Boolean(games.next)}
+        loadMore={() =>
+          fetch(data.next)
+            .then((res) => res.json())
+            .then((newData) => {
+              setData({
+                ...newData,
+                results: [...data.results, ...newData.results],
+              });
+            })
+        }
+        loader={<Loader key={data.results.length} />}
+      >
         <div
-          css={{
-            margin: 10,
-            textAlign: "center",
-            width: "45vw",
-            maxWidth: 420,
-            height: 200,
-            background: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5) 45%, rgba(0,0,0,0) 100%), url(${game.background_image}) top/cover no-repeat`,
-            display: "flex",
-            alignItems: "flex-end",
-            justifyContent: "center",
-          }}
-          key={game.id}
+          css={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
         >
-          <p css={{ color: "#E2E2E2" }}>{game.name}</p>
+          {data.results.map((game) => (
+            <div
+              css={{
+                margin: 10,
+                textAlign: "center",
+                width: "45vw",
+                maxWidth: 420,
+                height: 200,
+                background: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5) 45%, rgba(0,0,0,0) 100%), url(${game.background_image}) top/cover no-repeat`,
+                display: "flex",
+                alignItems: "flex-end",
+                justifyContent: "center",
+              }}
+              key={game.id}
+            >
+              <p css={{ color: "#E2E2E2" }}>{game.name}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </main>
-);
+      </InfiniteScroll>
+    </main>
+  );
+};
 
 Home.getInitialProps = async () => {
   const response = await fetch(
