8a38dca37aac6c0180d3c97dd4a8b7553e258e9c
SinshiSmith <yorfnd@gmail.com>
2020-12-22 14:33:20 +0200

    _app: add emotion css reference for auto-complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
-import { Fragment } from "react";
+/// <reference types="@emotion/react/types/css-prop" />;
 import { Global } from "@emotion/react";
 import Head from "next/head";
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <Fragment>
-      <Global
-        styles={{
-          body: {
-            margin: 0,
-            background: "#121212",
-            color: "#E2E2E2",
-            fontFamily: "'PT Sans', sans-serif",
-          },
-        }}
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Global
+      styles={{
+        body: {
+          margin: 0,
+          background: "#121212",
+          color: "#E2E2E2",
+          fontFamily: "'PT Sans', sans-serif",
+        },
+      }}
+    />
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
       />
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </Fragment>
-  );
-};
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
