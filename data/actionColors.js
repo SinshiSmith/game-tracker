cfef34e275b270094c98bba350339011979e465d
SinshiSmith <yorfnd@gmail.com>
2020-12-25 19:32:43 +0200

    create actionsColors

---
+++ data/actionColors.js
@@ -0,0 +1,8 @@
+const actionsColors = {
+  playing: { background: "#003791", color: "#E2E2E2" },
+  onHold: { background: "#000000", color: "#E2E2E2" },
+  dropped: { background: "#E60012", color: "#E2E2E2" },
+  backlog: { background: "#107C10", color: "#E2E2E2" },
+};
+
+export default actionsColors;
