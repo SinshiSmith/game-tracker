e3cf40b02106fd6c9e0a09305f19e6b11d9ced04
SinshiSmith <yorfnd@gmail.com>
2020-12-25 21:45:32 +0200

    actionColors: Add finished empty

--- data/actionColors.js
+++ data/actionColors.js
@@ -3,6 +3,7 @@ const actionsColors = {
   onHold: { background: "#000000", color: "#E2E2E2" },
   dropped: { background: "#E60012", color: "#E2E2E2" },
   backlog: { background: "#107C10", color: "#E2E2E2" },
+  finished: {},
 };
 
 export default actionsColors;
