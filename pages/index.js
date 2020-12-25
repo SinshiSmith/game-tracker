56d4149547e11421642331d9e4c03169b1477c06
SinshiSmith <yorfnd@gmail.com>
2020-12-25 19:57:52 +0200

    /home: Re-design to display player Games

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,112 @@
-import { useState } from "react";
-import InfiniteScroll from "react-infinite-scroller";
 import GameCard from "../components/GameCard";
 import Link from "../components/Link";
-import Loader from "../components/Loader";
+import useUser from "../hooks/useUser";
 
-const Home = ({ games = {} }) => {
-  const [data, setData] = useState(games);
+const Home = () => {
+  const [user] = useUser();
+  const games =
+    user.games?.reduce(
+      (gamesObject, current) => ({
+        ...gamesObject,
+        [current.list]: [...(gamesObject[current.list] || []), current],
+      }),
+      {}
+    ) || {};
+
   return (
-    <main css={{ padding: 10 }}>
-      <div css={{ textAlign: "center" }}>
-        <h1 css={{ margin: 0, fontWeight: 700, fontSize: 70 }}>Game Tracker</h1>
-        <p css={{ marginTop: 0 }}>a website to keep track of my games</p>
-      </div>
-      <InfiniteScroll
-        pageStart={0}
-        hasMore={Boolean(games.next)}
-        loadMore={() =>
-          fetch(data.next)
-            .then((res) => res.json())
-            .then((newData) => {
-              setData({
-                ...newData,
-                results: [...data.results, ...newData.results],
-              });
-            })
-        }
-        loader={<Loader key={data.results.length} />}
-      >
-        <div
-          css={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-          }}
-        >
-          {data.results.map((game) => (
-            <Link href={`/${game.id}`}>
-              <GameCard key={game.id} game={game} />
-            </Link>
-          ))}
-        </div>
-      </InfiniteScroll>
+    <main
+      css={{
+        padding: 10,
+        section: {
+          borderRadius: 5,
+          marginBottom: 50,
+          padding: "10px 30px",
+          background: "#000000",
+        },
+      }}
+    >
+      <section id="playing">
+        <h2>Currently Playing</h2>
+        {games.playing ? (
+          <div
+            css={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            {games.playing.map((game) => (
+              <Link href={`/${game.id}`}>
+                <GameCard key={game.id} game={game} />
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p css={{ textAlign: "center", marginBottom: 50 }}>Such Emptiness</p>
+        )}
+      </section>
+      <section id="onHold">
+        <h2>Games On Hold</h2>
+        {games.onHold ? (
+          <div
+            css={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            {games.onHold.map((game) => (
+              <Link href={`/${game.id}`}>
+                <GameCard key={game.id} game={game} />
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p css={{ textAlign: "center", marginBottom: 50 }}>Such Emptiness</p>
+        )}
+      </section>
+      <section id="backlog">
+        <h2>Up Next</h2>
+        {games.backlog ? (
+          <div
+            css={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            {games.backlog.map((game) => (
+              <Link href={`/${game.id}`}>
+                <GameCard key={game.id} game={game} />
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p css={{ textAlign: "center", marginBottom: 50 }}>Such Emptiness</p>
+        )}
+      </section>
+      <section id="dropped">
+        <h2>Dropped</h2>
+        {games.dropped ? (
+          <div
+            css={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            {games.dropped.map((game) => (
+              <Link href={`/${game.id}`}>
+                <GameCard key={game.id} game={game} />
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p css={{ textAlign: "center", marginBottom: 50 }}>Such Emptiness</p>
+        )}
+      </section>
     </main>
   );
-};
-
-Home.getInitialProps = async () => {
-  const response = await fetch(
-    `https://api.rawg.io/api/games?${process.env.NEXT_PUBLIC_API_KEY}`
-  );
-  const games = await response.json();
-  return { games };
 };
 
 export default Home;
