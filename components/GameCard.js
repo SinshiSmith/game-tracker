2c0194127894644cf30d8ed50c82825b5983a3be
SinshiSmith <yorfnd@gmail.com>
2020-12-25 17:47:31 +0200

    GameCard: replace text platform with icons

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,3 +1,5 @@
+import platforms from "../data/platforms";
+
 const GameCard = ({ game }) => (
   <div css={{ width: "100%", maxWidth: 200, margin: "10px 0" }}>
     <img
@@ -11,29 +13,20 @@ const GameCard = ({ game }) => (
         overflow: "hidden",
         textOverflow: "ellipsis",
         color: "#E2E2E2",
-        margin: "5px 0",
-        marginRight: 5,
+        margin: 0,
+        marginTop: 5,
       }}
     >
       {game.name}
     </p>
     <div css={{ display: "flex", flexWrap: "wrap" }}>
       {game.parent_platforms.map(({ platform }) => (
-        <p
+        <div
+          css={{ width: 14, fill: "#FFFFFF", marginRight: 5 }}
           key={platform.id}
-          css={{
-            margin: 0,
-            marginRight: 5,
-            marginBottom: 5,
-            padding: "2px 10px",
-            background: "#E2E2E2",
-            color: "#000000",
-            fontSize: 14,
-            borderRadius: 3,
-          }}
         >
-          {platform.name}
-        </p>
+          {platforms[platform.slug]?.icon}
+        </div>
       ))}
     </div>
   </div>
